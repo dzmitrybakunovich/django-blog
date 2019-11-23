99213e95ef20c400d89461d92cce9d9c6111f146
Dmitry <dv.bakunovich@gmail.com>
2019-11-23 12:19:17 +0300

    Added random selection for interactive

--- main_app/views.py
+++ main_app/views.py
@@ -2,17 +2,21 @@ from django.core.paginator import Paginator
 from django.db.models import Count
 from django.shortcuts import render, redirect
 from django.utils import timezone
+import random
 
 from .forms import ArticleForm, CommentForm
 from .models import Article, Comment, CustomUser
 
 
 def main_page(request):
-    all_articles = Article.objects.annotate(Count('comment')).order_by('-date')
-    main_articles = Article.objects.annotate(Count('comment')).order_by('-date')[:2]
-    main_article = Article.objects.annotate(Count('comment')).get(id=4)
     AMOUNT_LAST_COMMENTS = 4
     last_comments = Comment.objects.order_by('-date')[:AMOUNT_LAST_COMMENTS]
+    all_articles = Article.objects.annotate(Count('comment')).order_by('-date')
+    ids_for_articles = Article.objects.values_list('id', flat=True)
+    ids_for_articles = list(ids_for_articles)
+    AMOUNT_RANDOM_ARTICLES = 3
+    random_ids_for_article = random.sample(ids_for_articles, AMOUNT_RANDOM_ARTICLES)
+    main_article = Article.objects.annotate(Count('comment')).filter(id__in=random_ids_for_article)
     paginator = Paginator(all_articles, 8)
     page = request.GET.get('page')
     articles = paginator.get_page(page)
@@ -20,8 +24,8 @@ def main_page(request):
         request,
         'main_app/basic_page.html',
         {
-            'main_article': main_article,
-            'main_articles': main_articles,
+            'main_article': main_article[0],
+            'main_articles': main_article[1:3],
             'articles': articles,
             'comment': last_comments,
         },
