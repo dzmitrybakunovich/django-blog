a2dbdac8b1367688cc4f8a4df6569d17a901dd0b
DmitryBakunovich <dv.bakunovich@gmail.com>
2020-04-18 17:13:17 +0300

    Refactor urls, script.js

--- main_app/api/urls.py
+++ main_app/api/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
- path('article/', views.ArticleView.as_view()),
- path('article/<int:pk>', views.SingleArticleView.as_view()),
- path('user/', views.UserView.as_view()),
- path('user/<int:pk>', views.UserDetail.as_view())
+    path('article/', views.ArticleView.as_view()),
+    path('article/<int:pk>', views.SingleArticleView.as_view()),
+    path('user/', views.UserView.as_view()),
+    path('user/<int:pk>', views.UserDetail.as_view()),
 ]
